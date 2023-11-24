9024b9001fc45bf10e43782d8457a234ac071fa6
Isin Balci <isinmertbalci@gmail.com>
2023-11-24 14:13:48 -0600

    desired_pose is changed to a service in trajectory_generator

--- diffdrive/trajectory_generator.py
+++ diffdrive/trajectory_generator.py
@@ -2,10 +2,12 @@ import rclpy
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
 
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from diffdrive_msgs.msg import Pose2DStamped, Pose2DTrajectory
+from diffdrive_msgs.srv import Pose2DSrv
+from geometry_msgs.msg import Pose2D
 from std_srvs.srv import Empty
-from builtin_interfaces.msg import Duration, Time
+from std_msgs.msg import Bool
+from builtin_interfaces.msg import Time
 
 from .traj_gen import trajectoryGenerator
 
@@ -44,10 +46,10 @@ class trajGenNode(Node):
             )
 
         self.robot_name = self.get_parameter("robot_name").value
-        self.create_subscription(Pose2DStamped, 
-                                 self.robot_name+"/desired_pose", 
-                                 self.des_pose_callback, 
-                                 qos_profile=1)
+        # self.create_subscription(Pose2DStamped, 
+        #                          self.robot_name+"/desired_pose", 
+        #                          self.des_pose_callback, 
+        #                          qos_profile=1)
         self.create_subscription(Pose2DStamped, 
                                  self.robot_name+"/state", 
                                  self.state_callback, 
@@ -62,6 +64,12 @@ class trajGenNode(Node):
             self.robot_name+"/clear_traj", 
             self.clear_traj_callback
             )
+        
+        self.desired_pose_srv = self.create_service(
+            Pose2DSrv, 
+            self.robot_name+"/desired_pose",
+            self.des_pose_callback
+        )
         
         self.clock = self.get_clock()
         
@@ -99,15 +107,17 @@ class trajGenNode(Node):
         self.t_init = self.t_init_time.sec + (self.t_init_time.nanosec/1e9)
         return
 
-    def des_pose_callback(self, msg:Pose2DStamped) -> None:
+    def des_pose_callback(self, request:Pose2D, response) -> None:
         self.p_d = np.zeros(2)
         self.theta_d = 0.
-        self.p_d[0] = msg.pose.x
-        self.p_d[1] = msg.pose.y
-        self.theta_d = msg.pose.theta
+        self.p_d[0] = request.pose.x
+        self.p_d[1] = request.pose.y
+        self.theta_d = request.pose.theta
         self.desired_state_set = True
         self.get_logger().info("Desired pose is saved")
-        return
+        response.success = Bool()
+        response.success.data = True
+        return response
     
     def solveProb(self) -> None:
         if self.initial_state_set and self.desired_state_set:
