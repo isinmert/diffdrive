045cbd8654c4095d83864ca08ca96b6524e93cb3
Isin M. Balci <isinmertbalci@gmail.com>
2023-12-18 17:16:12 +0300

    trajectory_tracker base class is generated.

--- diffdrive/base_tracker.py
+++ diffdrive/base_tracker.py
@@ -22,8 +22,8 @@ class TrajectoryTracker(Node):
     Takes a trajectory and outputs desired longitudinal and angular velocity 
     for the vehicle
     """
-    def __init__(self):
-        super().__init__("trajectory_tracker")
+    def __init__(self, name:str=""):
+        super().__init__(name)
 
         self.declare_parameter("robot_name", "")
         self.declare_parameter("check_step", 1)
@@ -31,11 +31,6 @@ class TrajectoryTracker(Node):
         self.declare_parameter("debug_sec", 0.5)
         self.declare_parameter("safety_threshold", 0.2)
         self.declare_parameter("cmd_vel_pub_rate", 0.05)
-
-        # self.create_subscription(Pose2DTrajectory,
-        #     self.get_parameter("robot_name").value+"/trajectory", 
-        #     self.trajectory_callback, 
-        #     qos_profile=1)
         
         self.save_traj_srv = self.create_service(
             Pose2DTrajSrv, 
@@ -64,15 +59,6 @@ class TrajectoryTracker(Node):
         self.spline_y = None
         self.spline_theta = None
         self.trajectory_set = False
-
-        # Initialize pd controllers
-        Kp_x = 0.7
-        Kp_y = 0.7
-        Kd_x = 1.0
-        Kd_y = 1.0
-        self.PD_x = PDController("x_controller", kp=Kp_x, kd=Kd_x)
-        self.PD_y = PDController("y_controller", kp=Kp_y, kd=Kd_y)
-
         self.prev_cmd_vel = TwistStamped()
 
         self.clock = self.get_clock()
@@ -251,146 +237,10 @@ class TrajectoryTracker(Node):
 
     
     def cmd_vel_pub_callback(self):
-        cmd_vel_msg = Twist()
-        car_state_time = self.car_state.timestamp
-        car_state_x  = self.car_state.pose.x
-        car_state_y = self.car_state.pose.y
-        car_state_theta = self.car_state.pose.theta
-
-        rot_matrix = getRotationMatrix(car_state_theta)
-        DEBUG = self.get_parameter("debug").value
-        DEBUG_SEC = self.get_parameter("debug_sec").value
-        SAFETY_THRESHOLD = self.get_parameter("safety_threshold").value
-
-        # check the difference between the current time and last state time
-        cur_time = timeToFloat(self.get_clock().now().to_msg())
-        if abs(cur_time-timeToFloat(car_state_time)) >= SAFETY_THRESHOLD:
-            self.cmd_vel_publisher.publish(cmd_vel_msg)
-            if DEBUG:
-                self.get_logger("State is not observed!!", 
-                                throttle_duration_sec=DEBUG_SEC)
-            return
-
-        # Get Pos, vel and acceleration values from trajectory 
-        success, state_at_t = self._get_state_from_traj(car_state_time)
-        
-        if success == True:
-            self.get_logger().info("Trajectory is being tracked",
-                                   throttle_duration_sec=DEBUG_SEC)
-        else:
-            return
-        
-        if DEBUG:
-            self.get_logger().info("theta : {}".format(car_state_theta), 
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("rotation_matrix: {}".format(rot_matrix), 
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info(
-                "inv_rot_mat: {}".format(np.linalg.inv(rot_matrix)), 
-                throttle_duration_sec=DEBUG_SEC)
-
-        # Compute error values
-        err_x = state_at_t[0] - car_state_x
-        err_y = state_at_t[1] - car_state_y
-
-        # check whether the goal is reached
-        if (timeToFloat(car_state_time) > self.spline_x.x[-1] 
-            and abs(err_x) <= 1.0e-1
-            and abs(err_y) <= 1.0e-1):
-            self.cmd_vel_publisher.publish(cmd_vel_msg)
-            self.prev_cmd_vel.twist = cmd_vel_msg
-            self.prev_cmd_vel.header.stamp = self.clock.now().to_msg()
-            self.get_logger().info("Trajectory is completed!!!",
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.trajectory_set = False
-            self.trajectory = Pose2DTrajectory()
-            self.spline_x = None
-            self.spline_y = None
-            self.spline_theta = None
-
-        if DEBUG:
-            self.get_logger().info("traj_x : {}".format(state_at_t[0]), 
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("traj_y : {}".format(state_at_t[1]), 
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("car_state_x : {}".format(car_state_x), 
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("car_state_y : {}".format(car_state_y), 
-                                   throttle_duration_sec=DEBUG_SEC)
-        
-        self.get_logger().info("err_x : {}".format(err_x), 
-                                throttle_duration_sec=DEBUG_SEC)
-        self.get_logger().info("err_y : {}".format(err_y), 
-                                throttle_duration_sec=DEBUG_SEC)
-
-        # Compute u_x and u_y 
-        PD_x_output = self.PD_x.getOutput(err_x, car_state_time)
-        PD_y_output = self.PD_y.getOutput(err_y, car_state_time)
-        u_x = state_at_t[4] + PD_x_output
-        u_y = state_at_t[5] + PD_y_output
-
-        if DEBUG:
-            self.get_logger().info("pd_x_output : {}".format(PD_x_output), 
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("pd_y_output : {}".format(PD_y_output), 
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("ax : {}".format(state_at_t[4]),
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("ay : {}".format(state_at_t[5]),
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("u_x : {}".format(u_x),
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("u_y : {}".format(u_y),
-                                   throttle_duration_sec=DEBUG_SEC)
-
-
-        # Compute vdot and vw values 
-        dummy = np.linalg.inv(rot_matrix) @ np.array([u_x, u_y])
-        vdot = dummy[0]
-        v_times_w = dummy[1]
-        if DEBUG:
-            self.get_logger().info("vdot : {}".format(vdot), 
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("v_times_w : {}".format(v_times_w), 
-                                   throttle_duration_sec=DEBUG_SEC)
-
-        # Compute vk and wk 
-        EPS = 1e-3
-        EPS_dt = 0.2
-        cur_time = self.clock.now().to_msg()
-        dt_duration = timeDiff(cur_time, self.prev_cmd_vel.header.stamp)
-        dt = timeToFloat(dt_duration)
-        vkm1 = self.prev_cmd_vel.twist.linear.x
-        if dt <= EPS_dt:
-            vk = vkm1 + vdot * dt
-        else:
-            vk = 0.0
-        if vk >= EPS or vk <= -EPS:
-            wk = v_times_w/vk
-        else:
-            wk = 0.0
-
-        if DEBUG:
-            self.get_logger().info("dt : {}".format(dt),
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("vkm1 : {}".format(vkm1),
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("vk : {}".format(vk),
-                                   throttle_duration_sec=DEBUG_SEC)
-            self.get_logger().info("wk : {}".format(wk),
-                                   throttle_duration_sec=DEBUG_SEC)
-        
-        # write cmd_vel message 
-        cmd_vel_msg.linear.x = vk
-        cmd_vel_msg.angular.z = wk
-
-        # save previous cmd_vel
-        self.prev_cmd_vel.twist = cmd_vel_msg
-        self.prev_cmd_vel.header.stamp = self.clock.now().to_msg()
-
-        # Publish message
-        self.cmd_vel_publisher.publish(cmd_vel_msg)
-        return
+        """
+        Implement this function in children classes.
+        """
+        raise NotImplementedError("Implement this in the children classes.")
     
     
     def state_callback(self, msg:Pose2DStamped):
@@ -406,22 +256,6 @@ class TrajectoryTracker(Node):
         return
 
 def main():
-    # Initialize ros client
-    rclpy.init()
-
-    # Initialize a trajectory tracker node
-    traj_tracker = TrajectoryTracker()
-
-    try:
-        # spin
-        rclpy.spin(traj_tracker)
-    except (KeyboardInterrupt, ExternalShutdownException):
-        pass
-    finally:  
-        # Destroy node on shutdown
-        traj_tracker.destroy_node()
-        # Shutdown ros client
-        rclpy.try_shutdown()
     pass
 
 if __name__ == "__main__":
--- diffdrive/lyapunov_tracker.py
+++ diffdrive/lyapunov_tracker.py
@@ -1,242 +1,112 @@
 import rclpy
-from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
 
-from trajectory_msgs.msg import JointTrajectory
 from geometry_msgs.msg import Twist, TwistStamped
-from builtin_interfaces.msg import Time
-from diffdrive_msgs.msg import Pose2DStamped
-from .tracker_utils import (PDController, compareTime, timeDiff, timeToFloat, 
-                            getRotationMatrix)
-
-from typing import Tuple, List
+from diffdrive_msgs.msg import Pose2DStamped, Pose2DTrajectory
+from .tracker_utils import compareTime, timeDiff, timeToFloat, getRotationMatrix
+from .base_tracker import TrajectoryTracker
 
 import numpy as np
 
-class LyapunovTracker(Node):
+class LyapunovTracker(TrajectoryTracker):
     """
     Trajectory tracker for differential drive robot. 
     Takes a trajectory and outputs desired longitudinal and angular velocity 
     for the vehicle
     """
     def __init__(self):
-        super().__init__("trajectory_tracker")
+        super().__init__("lyapunov_tracker")
 
-        self.declare_parameter("robot_name", "")
-        self.declare_parameter("check_step", 1)
-        self.declare_parameter("debug", True)
-
-        self.declare_parameter("k_x", 0.2)
-        self.declare_parameter("k_y", 0.2)
+        # Define tracker specific parameters
+        self.declare_parameter("k_x", 0.5)
+        self.declare_parameter("k_y", 0.5)
         self.declare_parameter("k_theta", 0.2)
-
-        self.create_subscription(JointTrajectory,
-            self.get_parameter("robot_name").value+"/trajectory", 
-            self.trajectory_callback, 
-            qos_profile=1)
-        
-        self.cmd_vel_publisher = self.create_publisher(
-            Twist, 
-            self.get_parameter("robot_name").value + "/cmd_vel",
-            1
-        )
-
-        self.create_subscription(
-            Pose2DStamped, 
-            self.get_parameter("robot_name").value + "/state",
-            self.state_callback, 
-            qos_profile=1)
-        
-        self.trajectory = JointTrajectory()
-        self.trajectory_set = False
-
-        self.prev_cmd_vel = TwistStamped()
-
-        self.clock = self.get_clock()
-
-        # Create safety timer
-        self.create_timer(
-            self.get_parameter("check_step").value, 
-            self.timer_callback
-            )
-
-        pass
-
-    def trajectory_callback(self, trj_msg:JointTrajectory)->None:
-        """
-        Save observed trajectory if it's frame_id is different than the
-        saved trajectory.
-        """
-        if trj_msg.header.frame_id != self.trajectory.header.frame_id:
-            self.trajectory = trj_msg
-        self.trajectory_set = True
         return
-    
-    def timer_callback(self) -> None:
-        if self.trajectory_set:
-            cur_time = self.clock.now().to_msg()
-            prev_cmd_time = self.prev_cmd_vel.header.stamp
-            time_diff = timeDiff(cur_time, prev_cmd_time)
-            time_diff_float = timeToFloat(time_diff)
-            if time_diff_float >= self.get_parameter("check_step").value:
-                safety_msg = Twist()
-                self.cmd_vel_publisher.publish(safety_msg)
-                self.get_logger().info("Stopping message sent")
 
-        return
     
-    def _get_state_from_traj(self, t:Time, traj:JointTrajectory) \
-        -> Tuple[bool, List[float]]:
+    def cmd_vel_pub_callback(self):
         """
-        Get state and acceleration values at a timestep from a given 
-        JointTrajectory.
+        cmd_vel message generation for lypanunov trajectory tracking controller.
         """
-
-        res = [0.0 for _ in range(6)]
-        success = False
-        terminal = False
-        DEBUG = self.get_parameter("debug").value
-        if DEBUG:
-            logger = self.get_logger()
-
-        if self.trajectory_set:
-            pass
-        else:
-            # self.get_logger().info("Trajectory is not set")
-            return success, terminal, res
-
-        traj_points = traj.points
-        t0_traj = traj.header.stamp
-        if len(traj_points) == 0:
-            self.get_logger().warning("Trajectory is empty!!")
-            return success, res
-        elif len(traj_points) == 1:
-            self.get_logger().warning("Num traj_points = 1. Not sufficient!!")
-            return success, res
-        
-        if DEBUG:
-            logger.info("t0_traj_sec : {}".format(t0_traj.sec))
-            logger.info("t0_traj_nano : {}".format(t0_traj.nanosec))
-            logger.info("t_sec : {}".format(t.sec))
-            logger.info("t_nano : {}".format(t.nanosec))
-
-        t_traj_final = traj_points[-1].time_from_start
-        t_greater_t0 = compareTime(t, t0_traj)
-        if t_greater_t0:
-            t_rel = timeDiff(t, t0_traj)
-        else:
-            t_rel = Time()
-        t_less_t_final =  compareTime(t_traj_final, t_rel)
-        if t_less_t_final:
-            pass
-        else:
-            t_rel = t_traj_final
-            terminal = True
-
-        if terminal:
-            px_cur = traj_points[-1].positions[0]
-            py_cur = traj_points[-1].positions[1]
-            vx_cur = traj_points[-1].velocities[0]
-            vy_cur = traj_points[-1].velocities[1]
-            ax_cur = traj_points[-1].accelerations[0]
-            ay_cur = traj_points[-1].accelerations[1]
-        else:
-            for k in range(len(traj_points)-1):
-                t_l = traj_points[k].time_from_start
-                t_r = traj_points[k+1].time_from_start
-                if compareTime(t_rel, t_l) and compareTime(t_r, t_rel):
-                    break
-        
-            dt_time = timeDiff(t_rel, t_l)
-            dt_total_time = timeDiff(t_r, t_l)
-            dt = timeToFloat(dt_time)
-            dt_total = timeToFloat(dt_total_time)
-
-            pxk = traj_points[k].positions[0]
-            pyk = traj_points[k].positions[1]
-            vxk = traj_points[k].velocities[0]
-            vyk = traj_points[k].velocities[1]
-            axk = traj_points[k].accelerations[0]
-            ayk = traj_points[k].accelerations[1]
-
-            pxkp1 = traj_points[k+1].positions[0]
-            pykp1 = traj_points[k+1].positions[1]
-            vxkp1 = traj_points[k+1].velocities[0]
-            vykp1 = traj_points[k+1].velocities[1]
-            axkp1 = traj_points[k+1].accelerations[0]
-            aykp1 = traj_points[k+1].accelerations[1]
-
-            ay_diff = aykp1 - ayk
-            ax_diff = axkp1 - axk
-
-            j_x = ax_diff / dt_total
-            j_y = ay_diff / dt_total
-
-            px_cur = pxk + vxk*dt + axk*(dt**2)*0.5 + ((dt**3)/6) * j_x
-            py_cur = pyk + vyk*dt + ayk*(dt**2)*0.5 + ((dt**3)/6) * j_y
-            vx_cur = vxk + axk*dt + (dt**2) * 0.5 * j_x
-            vy_cur = vyk + ayk*dt + (dt**2) * 0.5 * j_y
-            ax_cur = axk + dt * j_x
-            ay_cur = ayk + dt * j_y
-
-            if DEBUG:
-                logger.info("k : {}".format(k))
-                logger.info("t_l_sec : {}".format(t_l.sec))
-                logger.info("t_l_nano : {}".format(t_l.nanosec))
-                logger.info("t_r_sec : {}".format(t_r.sec))
-                logger.info("t_r_nano : {}".format(t_r.nanosec))
-                logger.info("t_rel_sec : {}".format(t_rel.sec))
-                logger.info("t_rel_nano : {}".format(t_rel.nanosec))
-                logger.info("px_cur : {}".format(px_cur))
-                logger.info("py_cur : {}".format(py_cur))
-                logger.info("pxk : {}".format(pxk))
-                logger.info("pyk : {}".format(pyk))
-                logger.info("vxk : {}".format(vxk))
-                logger.info("vyk : {}".format(vyk))
-                logger.info("axk : {}".format(axk))
-                logger.info("ayk : {}".format(ayk))
-                logger.info("j_x : {}".format(j_x))
-                logger.info("j_y : {}".format(j_y))
-
-
-        res = [px_cur, py_cur, vx_cur, vy_cur, ax_cur, ay_cur]
-        success = True
-        return success, terminal, res
-    
-    def state_callback(self, msg:Pose2DStamped):
-
-        # Extract position and orientation of the robot from the state message
         cmd_vel_msg = Twist()
-        car_state_time = msg.timestamp
-        car_state_x  = msg.pose.x
-        car_state_y = msg.pose.y
-        car_state_theta = msg.pose.theta
+        car_state_time = self.car_state.timestamp
+        car_state_x = self.car_state.pose.x
+        car_state_y = self.car_state.pose.y
+        car_state_theta = self.car_state.pose.theta
 
-        # extract the controller parameters
+        logger = self.get_logger()
+
         k_x = self.get_parameter("k_x").value
         k_y = self.get_parameter("k_y").value
         k_theta = self.get_parameter("k_theta").value
 
         rot_matrix = getRotationMatrix(car_state_theta)
         DEBUG = self.get_parameter("debug").value
-        if DEBUG:
-            logger = self.get_logger()
+        DEBUG_SEC = self.get_parameter("debug_sec").value
+        SAFETY_THRESHOLD = self.get_parameter("safety_threshold").value
+
+        # check the difference between the current time and last state time
+        cur_time = timeToFloat(self.get_clock().now().to_msg())
+        if abs(cur_time-timeToFloat(car_state_time)) >= SAFETY_THRESHOLD:
+            self.cmd_vel_publisher.publish(cmd_vel_msg)
+            if DEBUG:
+                logger.info("State is not observed!!", 
+                            throttle_duration_sec=DEBUG_SEC)
+            return
 
         # Get Pos, vel and acceleration values from trajectory 
-        success, terminal, state_at_t = self._get_state_from_traj(
-            car_state_time, self.trajectory
-            )
-        
-        if success:
-            self.get_logger().info("Trajectory is being tracked")
+        success, state_at_t = self._get_state_from_traj(car_state_time)
+
+        if success == True:
+            logger.info("Trajectory is being tracked",
+                        throttle_duration_sec=DEBUG_SEC)
         else:
             return
         
         if DEBUG:
-            logger.info("theta : {}".format(car_state_theta))
-            logger.info("rotation_matrix: {}".format(rot_matrix))
-            logger.info("inv_rot_mat: {}".format(np.linalg.inv(rot_matrix)))
+            logger.info("theta : {}".format(car_state_theta), 
+                        throttle_duration_sec=DEBUG_SEC)
+            logger.info("rotation_matrix: {}".format(rot_matrix), 
+                        throttle_duration_sec=DEBUG_SEC)
+            logger.info("inv_rot_mat: {}".format(np.linalg.inv(rot_matrix)), 
+                        throttle_duration_sec=DEBUG_SEC)
 
+        # Compute error values
+        err_x = state_at_t[0] - car_state_x
+        err_y = state_at_t[1] - car_state_y
+
+        # check whether the goal is reached
+        if (timeToFloat(car_state_time) > self.spline_x.x[-1] 
+            and abs(err_x) <= 1.0e-1
+            and abs(err_y) <= 1.0e-1):
+            self.cmd_vel_publisher.publish(cmd_vel_msg)
+            self.prev_cmd_vel.twist = cmd_vel_msg
+            self.prev_cmd_vel.header.stamp = self.clock.now().to_msg()
+            logger.info("Trajectory is completed!!!",
+                        throttle_duration_sec=DEBUG_SEC)
+            self.trajectory_set = False
+            self.trajectory = Pose2DTrajectory()
+            self.spline_x = None
+            self.spline_y = None
+            self.spline_theta = None
+            return
+
+        if DEBUG:
+            logger.info("traj_x : {}".format(state_at_t[0]), 
+                        throttle_duration_sec=DEBUG_SEC)
+            logger.info("traj_y : {}".format(state_at_t[1]), 
+                        throttle_duration_sec=DEBUG_SEC)
+            logger.info("car_state_x : {}".format(car_state_x), 
+                        throttle_duration_sec=DEBUG_SEC)
+            logger.info("car_state_y : {}".format(car_state_y), 
+                        throttle_duration_sec=DEBUG_SEC)
+        
+        logger.info("err_x : {}".format(err_x), 
+                    throttle_duration_sec=DEBUG_SEC)
+        logger.info("err_y : {}".format(err_y), 
+                    throttle_duration_sec=DEBUG_SEC)
+        
+        # Compute cmd_vel
         # Get q_t, q_ref_t and compute q_err
         q_ref_t = np.zeros((3, 1))
         q_t = np.zeros((3, 1))
@@ -245,8 +115,8 @@ class LyapunovTracker(Node):
         q_t[2] = car_state_theta
         q_ref_t[0] = state_at_t[0]
         q_ref_t[1] = state_at_t[1]
-        vk_x_ref, vk_y_ref = state_at_t[2], state_at_t[3]
-        thetak_ref = np.arctan2(vk_y_ref, vk_x_ref)
+        vxk_ref, vyk_ref = state_at_t[2], state_at_t[3]
+        thetak_ref = np.arctan2(vyk_ref, vxk_ref)
         q_ref_t[2] = thetak_ref
         q_err_t = q_ref_t - q_t
         if DEBUG:
@@ -256,7 +126,7 @@ class LyapunovTracker(Node):
             logger.info("px_ref : {}".format(q_ref_t[0]))
             logger.info("py_ref : {}".format(q_ref_t[1]))
             logger.info("theta_ref : {}".format(thetak_ref))
-
+        
         # Compute error terms err_x, err_y, err_theta
         helper_matrix = np.eye(3)
         helper_matrix[0:2, 0:2] = getRotationMatrix(car_state_theta).T
@@ -267,12 +137,12 @@ class LyapunovTracker(Node):
             logger.info("err_x : {}".format(err_x))
             logger.info("err_y : {}".format(err_y))
             logger.info("err_theta : {}".format(err_theta))
-
+        
         # compute reference traj linear and angular velocity
-        vk_ref = np.sqrt(vk_x_ref**2 + vk_y_ref**2)
+        vk_ref = np.sqrt(vxk_ref**2 + vyk_ref**2)
         ak_x_ref, ak_y_ref = state_at_t[4], state_at_t[5]
         ak_ref_vec = np.array([ak_x_ref, ak_y_ref])
-        vk_ref_vec = np.array([vk_x_ref, vk_y_ref])
+        vk_ref_vec = np.array([vxk_ref, vyk_ref])
         tk_ref_vec = vk_ref_vec / vk_ref
 
         ak_lon_magnitude = np.dot(ak_ref_vec, tk_ref_vec)
@@ -286,11 +156,11 @@ class LyapunovTracker(Node):
             wk_ref = np.sign(np.cross(tk_ref_vec, ak_lat_vec)) * wk_magnitude 
         else:
             wk_ref = 0.
-
+        
         if DEBUG:
             logger.info("vk_ref : {}".format(vk_ref))
             logger.info("wk_ref : {}".format(wk_ref))
-
+        
         vk_fb = k_x * err_x
         helper_sin_err_theta = 1.0 # sin(err_theta)/err_theta
         EPS_THETA = 1e-1
@@ -302,16 +172,10 @@ class LyapunovTracker(Node):
         vk = (vk_ref * np.cos(err_theta)) + vk_fb
         wk = wk_ref + wk_fb
 
-        if terminal and max(err_x, err_y) <= 1e-1:
-            vk = 0.0
-            wk = 0.0
-            if DEBUG:
-                logger.info("goal is reached!!")
-
         if DEBUG:
             logger.info("vk : {}".format(vk))
             logger.info("wk : {}".format(wk))
-
+        
         cmd_vel_msg.linear.x = vk
         cmd_vel_msg.angular.z = wk
         self.cmd_vel_publisher.publish(cmd_vel_msg)
